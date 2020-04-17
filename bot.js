bab55d9e67bab454905dcfeb0616d124f2035576
benopop137 <63872244+benopop137@users.noreply.github.com>
2020-04-17 21:23:39 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,11 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("684122377541517339")
+setInterval(function() {
+channel.send(`الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه الدنيا حلوه`);
+}, 30)
+})
